da3eb60f3fbbb7bfc213a01a99d1bae89bd38cb4
Giulio Dolcetti <164858928+dolcettig@users.noreply.github.com>
2026-09-21 15:35:58 +0200

    removed surface tension

--- code/figures/figure_a2/reconstr_depth.m
+++ code/figures/figure_a2/reconstr_depth.m
@@ -1,17 +1,12 @@
 function depth = reconstr_depth(wavelength,vel,alpha,d_bounds,vel_profile)
 
 g=9.81;
-surf_tens=72.75e-03; %(N/m), ref. 20deg, from Vargaftik et al., 1983 - International tables of the surface tension of water, J.Phys.Chem.Ref.Data
-dens=998.2; %(kg/m^3), ref. 20deg
 
 % initialise depth
 depth = nan(length(wavelength),2); % allow for the presence of two possible depth solutions
 
 % wavenumner (rad/m)
 wavenumber = 2*pi./wavelength;
-
-% Bond number
-B = dens*g./(wavenumber.^2*surf_tens);
 
 if length(vel) < length(wavelength)
     vel = mean(vel)*ones(size(wavelength));
@@ -32,12 +27,11 @@ switch vel_profile
 
     case 'constant'
         % Us = sqrt( (1 + B)/B *g*d *tanh(k*d)/(k*d) ) e.g., Dolcetti et al. (2016), eq. (4)
-        fun = @(d)  (g*d) *(1 + dens*g/(surf_tens*k^2)) / (dens*g/(surf_tens*k^2))  *tanh(k*d)/(k*d) - v^2;
-        %              gd           (1 + B)                     B
+        fun = @(d)  (g*d)  *tanh(k*d)/(k*d) - v^2;
 
     case 'linear'
         % (1 - 2\beta)*Us^2 = c_i^2 e.g., Dolcetti et al. (2016), eq. (6)
-        fun = @(d) (g*d) *(1 + dens*g/(surf_tens*k^2)) / (dens*g/(surf_tens*k^2))   /(1-2*((m/2) *tanh(k.*d) ./ (k.*d)))   *tanh(k*d)/(k*d) - v^2;
+        fun = @(d) (g*d) /(1-2*((m/2) *tanh(k.*d) ./ (k.*d)))   *tanh(k*d)/(k*d) - v^2;
         %             gd           (1 + B)                     B                             1/(1-2\beta)
 
     case 'power'
